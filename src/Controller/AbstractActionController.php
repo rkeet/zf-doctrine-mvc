8c2f221662dbdd53c61b93b499a0589f52181ee8
Robin Keet <robin@keet.me>
2018-05-06 18:38:05 +0200

    Created first setup global AbstractActionController and extending AbstractDoctrineActionController

---
+++ src/Controller/AbstractActionController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Keet\Mvc\Controller;
+
+use Zend\I18n\Translator\TranslatorAwareTrait;
+use Zend\Mvc\Controller\AbstractActionController as ZendAbstractActionController;
+
+abstract class AbstractActionController extends ZendAbstractActionController
+{
+    use TranslatorAwareTrait;
+
+    /**
+     * Redirect to a route, or pass the url to the view for a javascript redirect
+     *
+     * @return mixed|\Zend\Http\Response
+     */
+    public function redirectToRoute ()
+    {
+        if ($this->getRequest()->isXmlHttpRequest()) {
+            return [
+                'redirect' => call_user_func_array([
+                    $this->url(), 'fromRoute'
+                ], func_get_args())
+            ];
+        }
+
+        return call_user_func_array([
+            $this->redirect(), 'toRoute'
+        ], func_get_args());
+    }
+
+    /**
+     * Gets all Trait's used by class and parent classes, see comments @link http://php.net/class_uses
+     *
+     * @param $class
+     * @param bool $autoload
+     * @return array
+     */
+    protected function givenClassAndParentTraits($class, $autoload = true) {
+        $traits = [];
+
+        // Get traits of all parent classes
+        while ($class = get_parent_class($class)) {
+            $traits = array_merge(class_uses($class, $autoload), $traits);
+        };
+
+        // Get traits of all parent traits
+        $traitsToSearch = $traits;
+        while (!empty($traitsToSearch)) {
+            $newTraits = class_uses(array_pop($traitsToSearch), $autoload);
+            $traits = array_merge($newTraits, $traits);
+            $traitsToSearch = array_merge($newTraits, $traitsToSearch);
+        };
+
+        foreach ($traits as $trait => $same) {
+            $traits = array_merge(class_uses($trait, $autoload), $traits);
+        }
+
+        return array_unique($traits);
+    }
+}
---
+++ src/Controller/AbstractDoctrineActionController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Keet\Mvc\Controller;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\Util\ClassUtils;
+use Doctrine\ORM\EntityNotFoundException;
+use Keet\Mvc\Entity\AbstractEntity;
+use Zend\Di\Exception\ClassNotFoundException;
+
+abstract class AbstractDoctrineActionController extends AbstractActionController
+{
+    /**
+     * @var ObjectManager
+     */
+    protected $objectManager;
+
+    /**
+     * AbstractDoctrineController constructor.
+     * @param ObjectManager $objectManager
+     */
+    public function __construct(ObjectManager $objectManager)
+    {
+        $this->setObjectManager($objectManager);
+    }
+
+    /**
+     * Tests if a received class name is in fact a Doctrine Entity
+     *
+     * Based on @link https://stackoverflow.com/a/27121978/1155833
+     *
+     * @param string|object $classFQCN
+     * @return boolean
+     * @throws EntityNotFoundException
+     */
+    function isEntity(string $classFQCN): bool
+    {
+        if (!class_exists($classFQCN)) {
+
+            throw new ClassNotFoundException(sprintf('Given class FQCN "%s" does not exist.', $classFQCN));
+        }
+
+        if (!is_object($classFQCN)) {
+
+            throw new EntityNotFoundException(sprintf('Given class "%s" is not a Doctrine Entity.', $classFQCN));
+        }
+
+        return ! $this->getObjectManager()->getMetadataFactory()->isTransient(ClassUtils::getClass($classFQCN));
+    }
+
+    /**
+     * Gets the route parameters based on generic naming
+     * i.e. -> Giving a parameter ['id'] will cause $entity->getId() to be executed and returned as ['id' => 123]
+     *
+     * @param AbstractEntity $entity
+     * @param array $routeParams
+     * @return array
+     */
+    public function getRouteParams (AbstractEntity $entity, array $routeParams = [])
+    {
+        if (count($routeParams) === 0) {
+
+            return [];
+        }
+
+        $params = [];
+        foreach ($routeParams as $param) {
+            if (!method_exists($entity, 'get' . ucfirst($param))) {
+
+                trigger_error('Function "' . 'get' . ucfirst($param) . '" does not exist on "' . get_class($entity) . '". Skipping.');
+                continue;
+            }
+
+            $params[$param] = $entity->{'get' . ucfirst($param)}();
+        }
+
+        return $params;
+    }
+
+    /**
+     * @return ObjectManager
+     */
+    public function getObjectManager(): ObjectManager
+    {
+        return $this->objectManager;
+    }
+
+    /**
+     * @param ObjectManager $objectManager
+     * @return AbstractDoctrineActionController
+     */
+    public function setObjectManager(ObjectManager $objectManager): AbstractDoctrineActionController
+    {
+        $this->objectManager = $objectManager;
+        return $this;
+    }
+}
