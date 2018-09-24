fcca3b0452e1612156a14326f025f7eda1974808
Robin Keet <robin@keet.me>
2018-09-24 22:34:23 +0200

    Added EnabledTrait - Can be used by Entity classes to add an enabled true/false flag to records.

---
+++ src/Traits/EnabledTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Keet\Mvc\Traits;
+
+trait EnabledTrait
+{
+    /**
+     * Defaults to true.
+     *
+     * @var bool
+     * @Doctrine\ORM\Mapping\Column(name="enabled", type="boolean", nullable=false)
+     */
+    protected $enabled = true;
+
+    /**
+     * @return bool
+     */
+    public function isEnabled() : bool
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @param bool $enabled
+     * @return $this
+     */
+    public function setEnabled(bool $enabled)
+    {
+        $this->enabled = $enabled;
+        return $this;
+    }
+
+}
